2c07735132e5f9e2c6acd1233de4a4bb1563f294
BaoTQ15 <tranquocbao892@gmail.com>
2023-03-12 23:28:25 +0700

    add file sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int num1, num2;
+    
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    std::cout << "Sum of two number2: " << num1 + num2 << std::endl;
+
+    return 0;
+}
